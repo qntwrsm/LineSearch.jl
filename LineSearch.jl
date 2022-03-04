96ff108919ddfb7dd15818247d2f331014e662eb
Quint Wiersma <qnt.wrsm@gmail.com>
2022-03-04 11:28:54 +0100

    Moved backtracking linesearch structs to backtrack.jl

--- LineSearch.jl
+++ LineSearch.jl
@@ -19,18 +19,6 @@ export
     # Backtracking
     backtrack!
 
-# Structs
-Base.@kwdef mutable struct BackTrack{Tf, Gf, Ti}
-    α::Tf	            # stepsize
-	α_init::Tf 	        # initial stepsize
-    ρ::Gf = .5		    # contraction factor
-    c_1::Tf = 1e-4      # sufficient decrease constant
-    order::Ti = 0       # interpolation order
-    ρ_hi::Gf = .5       # safeguard upper bound
-    ρ_lo::Gf = .1       # safeguard lower bound
-    max_iter::Ti = 1000 # max iterations
-end
-
 # Include programs
 include("backtrack.jl")
 
--- backtrack.jl
+++ backtrack.jl
@@ -10,6 +10,18 @@ backtrack.jl
 @date: 2022/02/02
 =#
 
+# Backtracking struct
+Base.@kwdef mutable struct BackTrack{Tf, Gf, Ti}
+    α::Tf	            # stepsize
+	α_init::Tf 	        # initial stepsize
+    ρ::Gf = .5		    # contraction factor
+    c_1::Tf = 1e-4      # sufficient decrease constant
+    order::Ti = 0       # interpolation order
+    ρ_hi::Gf = .5       # safeguard upper bound
+    ρ_lo::Gf = .1       # safeguard lower bound
+    max_iter::Ti = 1000 # max iterations
+end
+
 """
     fixed!(ls, x, x_prev, p, f_prev, ∇f_prev, f, args)
 
